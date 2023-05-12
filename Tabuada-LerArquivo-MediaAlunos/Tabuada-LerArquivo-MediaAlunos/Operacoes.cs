494d9ebdf008507984fa0991db7bbb035b6557d6
jeronimo c l ramos cruz de lima ramos <dacruz1990@hotmail.com>
2023-05-11 21:59:09 -0300

    projeto de teste

---
+++ Tabuada-LerArquivo-MediaAlunos/Tabuada-LerArquivo-MediaAlunos/Operacoes.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace Tabuada_LerArquivo_MediaAlunos
+{
+    public static class Operacoes
+    {
+        public static void Tabuada(int numero)
+        {
+            Console.WriteLine("=== o resutado da tabuada de " + numero + " é ");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(numero + " x " + i + " = " + (numero * i));
+            }
+
+        }
+
+        public static void LerArquivo(string nomeArquivo)
+        {
+            using (var arquivo = File.OpenText(nomeArquivo))
+            {
+                var linha = string.Empty;
+                while ((linha = arquivo.ReadLine()) != null)
+                {
+                    Console.WriteLine(linha);
+                }
+            }
+        }
+
+        public static void CalcularMedia()
+        {
+            Console.WriteLine("Digite as notas dos tres semestres.");
+            Console.WriteLine("Digite a primeira nota.");
+            var primeiraNota = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a segunda nota.");
+            var segundaNota = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a terceira nota.");
+            var terceiraNota = Convert.ToDouble(Console.ReadLine());
+
+            var resultado = 0M;
+            resultado = Convert.ToDecimal((primeiraNota + segundaNota + terceiraNota) / 3);
+            if (resultado >= 5)
+            {
+                Console.WriteLine("Sua media Final é de = " + resultado.ToString("N1", new CultureInfo("pt-BR")));
+                Console.WriteLine("Voce esta aprovado.");
+                return;
+            }
+
+            Console.WriteLine("Sua media Final é de = " + resultado.ToString("N1", new CultureInfo("pt-BR")));
+            Console.WriteLine("Voce esta reprovado.");
+        }
+    }
+}
+
---
+++ Tabuada-LerArquivo-MediaAlunos/Tabuada-LerArquivo-MediaAlunos/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Tabuada_LerArquivo_MediaAlunos
+{
+    internal class Program
+    {
+        // variavel costante
+        public const int SAIDA_PROGRAMA = 0;
+        public const int LER_ARQUIVO = 1;
+        public const int TABUADA = 2;
+        public const int CALCULAR_MEDIA = 3;
+        public const int LIMPAR = 4;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+            inicio:
+
+                var mensagem = @"Digite uma das opções abaixo: 
+                               0 - Sair do programa: 
+                               1 - Ler Arquivo: 
+                               2 - Tabuada:
+                               3 - Caucular Media:
+                               4 - Limpar:";
+
+                Console.WriteLine("================================================");
+
+                Console.WriteLine(mensagem);
+
+                var opcaoEscolhida = int.Parse(Console.ReadLine());
+
+
+                if (opcaoEscolhida == SAIDA_PROGRAMA)
+                    break;
+
+                if (opcaoEscolhida == LIMPAR)
+                {
+                    Console.Clear();
+                    continue;
+                }
+
+                if (opcaoEscolhida == LER_ARQUIVO)
+                {
+                    Console.WriteLine("Opção le arquivo escolhida.");
+                    Operacoes.LerArquivo(@"C:\arquivos\arq1.txt");
+                    continue;
+                }
+
+                if (opcaoEscolhida == TABUADA)
+                {
+                    Console.WriteLine("Opção da tabuada escolhida.");
+                    Console.WriteLine("Digite um numero.");
+                    var numero = int.Parse(Console.ReadLine());
+                    Operacoes.Tabuada(numero);
+                    continue;
+                }
+
+                if (opcaoEscolhida == CALCULAR_MEDIA)
+                {
+                    Operacoes.CalcularMedia();
+                    continue;
+                }
+
+                Console.WriteLine("Opção invalida digite novamaente.");
+                goto inicio;
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+    
+
